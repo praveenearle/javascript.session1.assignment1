b34db733a8f91deb19088864ac442cbdb8ddd892
praveenearle <praveen.earle@gmail.com>
2016-08-19 22:02:49 +0530

    javascript s1a1
    javascript s1a1

---
+++ javascript s1a1/WebApplication4/circle-area.js
@@ -0,0 +1,10 @@
+﻿function calccirclearea(r)
+{
+    var area = (3.14*r*r);
+    document.write("radius is:" + r);
+    document.write("; ");
+    document.write("area  is:" + area);
+    document.write("<br/>");
+    document.write("<br/>");
+}
+ 
